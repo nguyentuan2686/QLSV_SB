3a42e334ecc9e2a931ade99a7e7389d844204f77
sonnguyenvan9794@gmail.com <sonnguyenvan9794@gmail.com>
2021-07-28 19:35:36 +0700

    khoi tao

---
+++ src/main/java/com/example/qlsv/QlsvApplication.java
@@ -0,0 +1,14 @@
+package com.example.qlsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+public class QlsvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QlsvApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/qlsv/entity/UserEntity.java
@@ -0,0 +1,27 @@
+package com.example.qlsv.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class UserEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+
+}
